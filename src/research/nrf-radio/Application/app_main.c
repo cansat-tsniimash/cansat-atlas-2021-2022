b38002e62774b6f8c9b1d8989f2380927cfdd293
@man-GO-228 <t.ozhigow@gmail.com>
2022-01-26 20:46:21 +0300

    Продолжаем работать над радио

--- src/research/nrf-radio/Application/app_main.c
+++ src/research/nrf-radio/Application/app_main.c
@@ -3,6 +3,189 @@
 
 extern SPI_HandleTypeDef hspi2;
 
+typedef struct
+{
+	struct
+	{
+		uint8_t mask_rx_dr:0;
+		uint8_t	mask_tx_ds:0;
+		uint8_t	mask_max_rt:0;
+		uint8_t	en_crc:1;
+		uint8_t	crc0:0;
+		uint8_t	pwr_up:0;
+		uint8_t	prim_rx:0;
+	} config;
+
+	struct
+	{
+		uint8_t enaa_p5:1;
+		uint8_t	enaa_p4:1;
+		uint8_t	enaa_p3:1;
+		uint8_t	enaa_p2:1;
+		uint8_t	enaa_p1:1;
+		uint8_t	enaa_p0:1;
+	} en_aa;
+
+	struct
+	{
+		uint8_t erx_p5:0;
+		uint8_t	erx_p4:0;
+		uint8_t	erx_p3:0;
+		uint8_t	erx_p2:0;
+		uint8_t	erx_p1:1;
+		uint8_t	erx_p0:1;
+	} en_aa;
+
+    struct
+	{
+		uint8_t aw:3;
+    } setup_aw;
+
+	struct
+	{
+		uint8_t ard:0;
+		uint8_t	arc:3;
+	} setup_retr;
+
+	struct
+	{
+		uint8_t rf_ch:2;
+	} rf_ch;
+
+	struct
+	{
+		uint8_t pll_lock:0;
+		uint8_t	rf_dr:1;
+		uint8_t	rf_pwr:3;
+		uint8_t	lna_hcurr:1;
+	} rf_setup;
+
+	struct
+	{
+		uint8_t en_dpl:0;
+		uint8_t	en_ack_pay:0;
+		uint8_t	en_dyn_ack:0;
+	} feature;
+
+	uint8_t rx_addr_p0[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
+	uint8_t rx_addr_p1[5] = {0xC2, 0xC2, 0xC2, 0xC2, 0xC2};
+	uint8_t rx_addr_p2 = 0xC3;
+	uint8_t rx_addr_p3 = 0xC4;
+	uint8_t rx_addr_p4 = 0xC5;
+	uint8_t rx_addr_p5 = 0xC6;
+
+	uint8_t tx_addr[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
+
+	struct
+	{
+		uint8_t rx_pw_p0:0;
+	} rx_pw_p0;
+
+	struct
+	{
+		uint8_t rx_pw_p1:0;
+	} rx_pw_p1;
+
+	struct
+	{
+		uint8_t rx_pw_p2:0;
+	} rx_pw_p2;
+
+	struct
+	{
+		uint8_t rx_pw_p3:0;
+	} rx_pw_p3;
+
+	struct
+	{
+		uint8_t rx_pw_p4:0;
+	} rx_pw_p4;
+
+	struct
+	{
+		uint8_t rx_pw_p5:0;
+	} rx_pw_p5;
+
+
+	struct
+	{
+		uint8_t dpl_p5:0;
+		uint8_t	dpl_p4:0;
+		uint8_t	dpl_p3:0;
+		uint8_t	dpl_p2:0;
+		uint8_t	dpl_p1:0;
+		uint8_t	dpl_p0:0;
+	} dynpd;
+}nrf24l01_config_t;
+
+#define STATUS 0x07
+
+#define CONFIG 0x00
+#define CONFIG_MASK_RX_DR  6
+#define CONFIG_MASK_TX_DS  5
+#define CONFIG_MASK_MAX_RT 4
+#define CONFIG_EN_CRC      3
+#define CONFIG_CRCO        2
+#define CONFIG_PWR_UP      1
+#define CONFIG_PRIM_RX     0
+
+#define EN_AA 0x01
+#define EN_AA_ENAA_P5	5
+#define EN_AA_ENAA_P4	4
+#define EN_AA_ENAA_P3	3
+#define EN_AA_ENAA_P2	2
+#define EN_AA_ENAA_P1	1
+#define EN_AA_ENAA_P0	0
+
+#define EN_RXADDR 0x01
+#define EN_RXADDR_ERX_P5	5
+#define EN_RXADDR_ERX_P4	4
+#define EN_RXADDR_ERX_P3	3
+#define EN_RXADDR_ERX_P2	2
+#define EN_RXADDR_ERX_P1	1
+#define EN_RXADDR_ERX_P0	0
+
+#define SETUP_AW 0x03
+
+#define SETUP_RETR 0x04
+#define SETUP_RETR_ARD 4
+#define SETUP_RETR_ARC 0
+
+#define RF_CH 0x05
+
+#define RF_SETUP 0x06
+#define RF_SETUP_PLL_LOCK 0
+#define RF_SETUP_RF_DR 1
+#define RF_SETUP_RF_PWR 11
+#define RF_SETUP_LNA_HCURR 1
+
+#define RX_ADDR_P0 0A
+#define RX_ADDR_P1 0B
+#define RX_ADDR_P2 0C
+#define RX_ADDR_P3 0D
+#define RX_ADDR_P4 0E
+#define RX_ADDR_P5 0F
+
+#define TX_ADDR 0x10
+
+#define RX_PW_P0 0x11
+#define RX_PW_P1 0x12
+#define RX_PW_P2 0x13
+#define RX_PW_P3 0x14
+#define RX_PW_P4 0x15
+#define RX_PW_P5 0x16
+
+#define FIFO_STATUS 0x17
+
+#define DYNPD 0x1C
+
+#define FEATURE 0x1D
+
+
+
+
+
+
 static uint8_t read_reg(uint8_t reg_addr, uint8_t *reg_data, uint32_t len)
 {
 	// Переменная для значения STATUS регистра
@@ -58,6 +241,23 @@ static void send_command(uint8_t command){
 
 	// Поднимаем chip select для того, что бы закончить общение с конкретным устройством.
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1,  GPIO_PIN_SET);
+}
+
+void nrf24l01_get_config(nrf24l01_config_t *nrf_config)
+{
+	uint8_t reg;
+	read_reg(CONFIG, &reg, 1);
+
+	nrf_config->config.mask_rx_dr = (reg >> CONFIG_MASK_RX_DR) & 0x01;
+	nrf_config->config.mask_tx_ds = (reg >> CONFIG_MASK_TX_DS) & 0x01;
+	nrf_config->config.mask_max_rt = (reg >> CONFIG_MASK_MAX_RT) & 0x01;
+	nrf_config->config.en_crc = (reg >> CONFIG_EN_CRC) & 0x01;
+	nrf_config->config.crc0 = (reg >> CONFIG_CRCO) & 0x01;
+	nrf_config->config.prim_rx = (reg >> CONFIG_PRIM_RX) & 0x01;
+
+	read_reg(CONFIG, &reg, 1);
+
+	nrf_config->
 }
 
 int app_main()
---
+++ src/research/nrf-radio/Application/app_main.h
@@ -0,0 +1,74 @@
+/*
+ * app_main.h
+ *
+ *  Created on: 26 янв. 2022 г.
+ *      Author: 1
+ */
+
+#ifndef APP_MAIN_H_
+#define APP_MAIN_H_
+
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+
+void read_reg(uint8_t reg_addr, uint8_t * data, size_t data_size);
+void write_reg(uint8_t reg_addr, const uint8_t * data, size_t data_size);
+void read_rx_payload(uint8_t * buffer, suze_t buffer_size);
+void write_tx_payload(const uint8_t * buffer, size_t buffer_size, bool use_ak);
+void flush_tx(void);
+void flush_rx(void);
+void reuse_tx_pl(void);
+void activate_features(void);
+void read_plw(void);
+void write_ak_pl(const uint8_t * buffer, size_t size);
+void get_status(uint8_t * status);
+
+
+int read_pl(uint8_t * pl , size_t);
+int peek_rx_pl(void);
+
+typedef enum mode_t
+{
+	MODE_TX,
+	MODE_RX
+} mode_t;
+
+int switch_mode(mode_t mode);
+
+int set_tx_pipe_addr(uint64_t pipe_addr);
+
+int set_freq();
+
+
+typedef enum data_rate_t
+{
+	DATA_RATE_250KBPS,
+	DATA_RATE_1MBPS,
+	DATA_RATE_2MBPS
+} data_rate_t;
+
+
+typedef enum rf_power_t
+{
+	RF_POWER_0DBM,
+	RF_POWER_MINUS_6DBM,
+	RF_POWER_MINUS_12DBM,
+	RF_POWER_MINUS_18DBM,
+} rf_power_t;
+
+
+typedef struct rf_params_t
+{
+	uint8_t rf_channel;
+	data_rate_t data_rate;
+	rf_power_t power;
+	bool enable_lna;
+} rf_params_t;
+
+void
+
+
+#endif /* APP_MAIN_H_ */
