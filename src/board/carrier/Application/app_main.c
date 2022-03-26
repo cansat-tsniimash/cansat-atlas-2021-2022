68255e64c39129b23a98feadc76e396547ed10c6
@man-GO-228 <t.ozhigow@gmail.com>
2022-03-26 20:16:38 +0300

    прикрутили UART, дописали код для запроса данных с ДА

--- src/board/carrier/Application/app_main.c
+++ src/board/carrier/Application/app_main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stm32f4xx_hal.h>
+#include <stdio.h>
 #include "motor.h"
 #include "triggers.h"
 #include "config.h"
@@ -8,6 +9,14 @@
 #include "nRF24L01_PL/nrf24_lower_api_stm32.h"
 
 extern SPI_HandleTypeDef hspi2;
+
+typedef enum//ОПИСЫВАЕМ ОБЩЕНИЕ ПО РАДИО
+{
+    STATE_BUILD_PACKET_TO_GCS,
+	STATE_SEND_PACKET_TO_GCS,
+	STATE_BUILD_PACKET_TO_DA_1,
+	STATE_SEND_PACKET_TO_DA_1
+}nrf24_state_t;
 
 typedef enum//ОПИСЫВАЕМ СОСТОЯНИЕ ПОЛЕТА
 {
@@ -77,6 +86,8 @@ int app_main()
 	nrf24_api_config.ce_pin = GPIO_PIN_1;
 	nrf24_api_config.cs_port = GPIOA;
 	nrf24_api_config.cs_pin = GPIO_PIN_0;
+
+	nrf24_mode_power_down(&nrf24_api_config);
 	// Настройки радиопередачи
 	nrf24_rf_config_t nrf24_rf_config;
 	nrf24_rf_config.data_rate = NRF24_DATARATE_2000_KBIT;
@@ -98,7 +109,7 @@ int app_main()
 	nrf24_pipe_set_tx_addr(&nrf24_api_config, 0xacacacacac);
 
 	nrf24_pipe_config_t pipe_config;
-	pipe_config.address = 0xacacacacac;
+	pipe_config.address = 0xafafafaf01;
 	pipe_config.enable_auto_ack = true;
 	pipe_config.payload_size = -1;
 	nrf24_pipe_rx_start(&nrf24_api_config, 0, &pipe_config);
@@ -106,9 +117,16 @@ int app_main()
 	nrf24_mode_standby(&nrf24_api_config);
 	nrf24_mode_tx(&nrf24_api_config);
 
+	uint8_t da_1_rx_buffer[32];
 	uint32_t start_time = 0;
 	state_t state_now = STATE_ON_GROUND;
+	nrf24_state_t nrf24_state_now = STATE_BUILD_PACKET_TO_GCS;
 	motor_on();
+	nrf24_fifo_status_t rx_status;
+	nrf24_fifo_status_t tx_status;
+	int comp;
+	uint8_t da_1_resp_count = 0;
+
 	while(1)
 	{
 		switch (state_now)
@@ -141,12 +159,62 @@ int app_main()
 			//общение с ДА
 			break;
 		}
-	    nrf24_fifo_write(&nrf24_api_config, (uint8_t *)&packet_ma_type_1, sizeof(packet_ma_type_1), false);
-	    nrf24_fifo_write(&nrf24_api_config, (uint8_t *)&packet_ma_type_2, sizeof(packet_ma_type_2), false);
 
+	    switch (nrf24_state_now)
+	    {
+	        case STATE_BUILD_PACKET_TO_GCS:
+	        	nrf24_pipe_set_tx_addr(&nrf24_api_config, 0xacacacacac);
+			    nrf24_fifo_write(&nrf24_api_config, (uint8_t *)&packet_ma_type_1, sizeof(packet_ma_type_1), false);
+			    nrf24_fifo_write(&nrf24_api_config, (uint8_t *)&packet_ma_type_2, sizeof(packet_ma_type_2), false);
+			    nrf24_state_now = STATE_SEND_PACKET_TO_GCS;
+			    break;
+	        case STATE_SEND_PACKET_TO_GCS:
+	     	    nrf24_fifo_status(&nrf24_api_config, &rx_status, &tx_status);
+	            if(tx_status == NRF24_FIFO_EMPTY)
+	            {
+	            	nrf24_state_now = STATE_BUILD_PACKET_TO_DA_1;
+	            	da_1_resp_count = 0;
+	            }
+	            break;
+	        case STATE_BUILD_PACKET_TO_DA_1 :
+	        	da_1_resp_count++;
+	        	printf("ДА, лови маслину\n");
+	        	nrf24_pipe_set_tx_addr(&nrf24_api_config, 0xafafafaf01);
+			    nrf24_fifo_write(&nrf24_api_config, (uint8_t *)&packet_ma_type_1, sizeof(packet_ma_type_1), true);
+			    nrf24_state_now = STATE_SEND_PACKET_TO_DA_1;
+	        case STATE_SEND_PACKET_TO_DA_1:
+	        	nrf24_irq_get(&nrf24_api_config, &comp);
+
+	        	if (comp & NRF24_IRQ_TX_DR)
+	        	{
+	        		size_t packet_size = nrf24_fifo_read(&nrf24_api_config, da_1_rx_buffer, sizeof(da_1_rx_buffer));
+	        		if (packet_size > 0)
+	        		{
+	        			for(int i =0 ; i < packet_size ; i++)
+	        			{
+	        				printf("%i ", da_1_rx_buffer[i]);
+	        			}
+	        			printf("\n");
+	        		}
+	        		else
+	        		{
+        				printf("пакета нету, гуляй\n");
+	        		}
+	        		if (da_1_resp_count >= 2)
+	        			nrf24_state_now = STATE_BUILD_PACKET_TO_GCS;
+	        		else
+						nrf24_state_now = STATE_BUILD_PACKET_TO_DA_1;
+	        	}
+	        	if (comp & NRF24_IRQ_MAX_RT)
+	        	{
+	        		printf("!!!ОШИБКА!!!\n");
+	        		if (da_1_resp_count > 2)
+	        			nrf24_state_now = STATE_BUILD_PACKET_TO_GCS;
+	        		else
+						nrf24_state_now = STATE_BUILD_PACKET_TO_DA_1;
+	        	}
+	    }
 		nrf24_irq_clear(&nrf24_api_config, NRF24_IRQ_RX_DR | NRF24_IRQ_TX_DR | NRF24_IRQ_MAX_RT);
-
-
 	}
 	return 0;
 }
