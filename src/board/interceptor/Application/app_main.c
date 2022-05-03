314723fc1b5bc682b2c1aa52df2d1347cb0a2f48
@man-GO-228 <t.ozhigow@gmail.com>
2022-05-03 20:18:10 +0300

    Актуализировали датчики и радио

--- src/board/interceptor/Application/app_main.c
+++ src/board/interceptor/Application/app_main.c
@@ -10,10 +10,13 @@
 #include "ATGM336H/nmea_gps.h"
 #include <math.h>
 #include "BME280/DriverForBME280.h"
+#include "../stm32f1/LSM6DS3/DLSM.h"
 
 extern UART_HandleTypeDef huart3;
 
 extern SPI_HandleTypeDef hspi1;
+extern SPI_HandleTypeDef hspi2;
+
 
 #pragma pack(push,1)
 typedef struct
@@ -71,6 +74,13 @@ int app_main()
 	bme280_buffer.spi =  &hspi1;
 	bme_init_default(&bme, &bme280_buffer);
 
+	struct lsm_spi_intf lsm_intf;
+	lsm_intf.GPIO_Pin = GPIO_PIN_14;
+	lsm_intf.GPIO_Port = GPIOC;
+	lsm_intf.spi = &hspi1;
+
+	stmdev_ctx_t stmdev_ctx;
+	lsmset(&stmdev_ctx, &lsm_intf);
 
 	//инициализация гпс
 	gps_init();
@@ -84,21 +94,24 @@ int app_main()
 	packet_da_type_1.flag = 0xfa;
 	packet_da_type_2.flag = 0xfb;
 
-	nrf24_lower_api_config_t nrf24_api_config;
-	nrf24_api_config.hspi = &hspi1;
-	nrf24_api_config.ce_port = GPIOA;
-	nrf24_api_config.ce_pin = GPIO_PIN_10;
-	nrf24_api_config.cs_pin = GPIO_PIN_15;
-	nrf24_api_config.cs_port = GPIOC;
+	nrf24_spi_pins_t nrf24_spi_pins;
+	nrf24_spi_pins.ce_port = GPIOA;
+	nrf24_spi_pins.ce_pin = GPIO_PIN_10;
+	nrf24_spi_pins.cs_pin = GPIO_PIN_15;
+	nrf24_spi_pins.cs_port = GPIOC;
 
-	nrf24_mode_power_down(&nrf24_api_config);
+	nrf24_lower_api_config_t nrf24_lower_api_config;
+
+	nrf24_spi_init(&nrf24_lower_api_config, &hspi2, &nrf24_spi_pins);
+
+	nrf24_mode_power_down(&nrf24_lower_api_config);
 
 	// Настройки радиопередачи
 	nrf24_rf_config_t nrf24_rf_config;
 	nrf24_rf_config.data_rate = NRF24_DATARATE_2000_KBIT;
 	nrf24_rf_config.rf_channel = 25;
 	nrf24_rf_config.tx_power = NRF24_TXPOWER_MINUS_0_DBM;
-	nrf24_setup_rf(&nrf24_api_config, &nrf24_rf_config);
+	nrf24_setup_rf(&nrf24_lower_api_config, &nrf24_rf_config);
 
 	// Настроили протокол
 	nrf24_protocol_config_t nrf24_protocol_config;
@@ -109,27 +122,31 @@ int app_main()
 	nrf24_protocol_config.en_ack_payload = true;
 	nrf24_protocol_config.en_dyn_ack = true;
 	nrf24_protocol_config.en_dyn_payload_size = true;
-	nrf24_setup_protocol(&nrf24_api_config, &nrf24_protocol_config);
+	nrf24_setup_protocol(&nrf24_lower_api_config, &nrf24_protocol_config);
 
 	//настройка пайпа(штука , чтобы принимать)
 	nrf24_pipe_config_t pipe_config;
 	pipe_config.address = 0xafafafaf01;
 	pipe_config.enable_auto_ack = true;
 	pipe_config.payload_size = -1;
-	nrf24_pipe_rx_start(&nrf24_api_config, 0, &pipe_config);
+	nrf24_pipe_rx_start(&nrf24_lower_api_config, 0, &pipe_config);
 
-	nrf24_mode_standby(&nrf24_api_config);
+	nrf24_mode_standby(&nrf24_lower_api_config);
 
 	uint8_t rx_buffer[32];
 
 	nrf24_fifo_status_t rx_status = 0;
     nrf24_fifo_status_t tx_status = 0;
-	nrf24_mode_rx(&nrf24_api_config);
+	nrf24_mode_rx(&nrf24_lower_api_config);
 
 	int64_t cookie;
 	struct bme280_data comp_data = {0};
 	comp_data = bme_read_data(&bme);
 	pressure_on_ground = (float)comp_data.pressure;
+
+	float temperature_celsius_gyro = 0;
+    float acc_g [3];
+    float gyro_dps [3];
 
 	float height_on_BME280 = 0;
 	while(true)
@@ -147,26 +164,28 @@ int app_main()
 		printf("темп %ld\n ",(int32_t)packet_da_type_1.BME280_temperature);
 		printf("влажность %ld\n ",(int32_t)packet_da_type_1.BME280_humidity);
 
+		lsmread(&stmdev_ctx, &temperature_celsius_gyro, &acc_g, &gyro_dps);
+
 		gps_work();
 		gps_get_coords(&cookie, &packet_da_type_2.latitude, &packet_da_type_2.longitude, &packet_da_type_2.height);
 
 
-		nrf24_fifo_status(&nrf24_api_config, &rx_status, &tx_status);
+		nrf24_fifo_status(&nrf24_lower_api_config, &rx_status, &tx_status);
 
 		if (rx_status != NRF24_FIFO_EMPTY)
 		{
-			nrf24_fifo_read(&nrf24_api_config, rx_buffer, 32);
+			nrf24_fifo_read(&nrf24_lower_api_config, rx_buffer, 32);
 		}
 
         if (tx_status != NRF24_FIFO_EMPTY)
         {
     	    //nrf24_fifo_flush_tx(&nrf24_api_config);
         }
-        nrf24_fifo_write_ack_pld(&nrf24_api_config, 0,(uint8_t *)&packet_da_type_1, sizeof(packet_da_type_1));
-        nrf24_fifo_write_ack_pld(&nrf24_api_config, 0,(uint8_t *)&packet_da_type_2, sizeof(packet_da_type_2));
+        nrf24_fifo_write_ack_pld(&nrf24_lower_api_config, 0,(uint8_t *)&packet_da_type_1, sizeof(packet_da_type_1));
+        nrf24_fifo_write_ack_pld(&nrf24_lower_api_config, 0,(uint8_t *)&packet_da_type_2, sizeof(packet_da_type_2));
 
         //опускаем флаги
-        nrf24_irq_clear(&nrf24_api_config, NRF24_IRQ_RX_DR | NRF24_IRQ_TX_DR | NRF24_IRQ_MAX_RT);
+        nrf24_irq_clear(&nrf24_lower_api_config, NRF24_IRQ_RX_DR | NRF24_IRQ_TX_DR | NRF24_IRQ_MAX_RT);
 
     }
 	return 0;
